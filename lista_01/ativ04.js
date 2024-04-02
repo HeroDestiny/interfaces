da71fd52b3110ada94d0608ce346ccf951de085f
Paulo Henrique Soares Lima e Silva <59665236+HeroDestiny@users.noreply.github.com>
2024-04-02 20:25:58 +0000

    aula02

--- lista_01/ativ04.js
+++
@@ -1,8 +0,0 @@
-const lado =  5; 
-
-const area = lado * lado;
-
-const perimetro =  4 * lado;
-
-console.log("Área: " + area);
-console.log("Perímetro: " + perimetro);
--- lista_01/ativ05.js
+++
@@ -1,8 +0,0 @@
-const numero = 5;
-
-let resultado =   1;
-for (let i =   1; i <= numero; i++) {
-    resultado *= i;
-}
-
-console.log(`O fatorial de ${numero} é: ${resultado}`);
